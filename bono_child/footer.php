868ab41418b7c58f03aee45b9b87893aaa26bdb1
Nikita Norenko <norenkonikita@gmail.com>
2022-03-19 23:42:33 +0200

    Функция поднятия наверх страницы

--- bono_child/footer.php
+++ bono_child/footer.php
@@ -41,7 +41,29 @@ if ($latest_posts) {
 <?php get_template_part('template-parts/footer/footer') ?>
 
 <?php if ($core->get_option('arrow_display')) : ?>
-	<button type="button" class="scrolltop js-scrolltop" <?php echo $core->get_option('arrow_mob_display') ? ' data-mob="on"' : '' ?>></button>
+	<button type="button" class="scrolltop js-scrolltop" <?php echo $core->get_option('arrow_mob_display') ? ' data-mob="on"' : '' ?>>
+		<svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g clip-path="url(#clip0_71_2697)">
+				<path d="M6.94214 6.68L1.77731 11.8548C1.37424 12.2629 0.714234 12.2629 0.306118 11.8548C-0.101968 11.4517 -0.101968 10.7917 0.306118 10.3836L6.20637 4.47333C6.40289 4.27681 6.66483 4.16602 6.94207 4.16602C7.21932 4.16602 7.48126 4.27681 7.67777 4.46832L13.5881 10.3886C13.7896 10.5901 13.8904 10.857 13.8904 11.1243C13.8904 11.3912 13.7896 11.6584 13.5881 11.86C13.18 12.268 12.525 12.268 12.1169 11.86L6.94214 6.68Z" fill="#F8F8F8" />
+			</g>
+			<defs>
+				<clipPath id="clip0_71_2697">
+					<rect width="14" height="14" fill="white" transform="translate(0 14.4995) rotate(-90)" />
+				</clipPath>
+			</defs>
+		</svg>
+		<svg class="scrolltop-down-arrow" width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<g clip-path="url(#clip0_71_2697)">
+				<path d="M6.94214 6.68L1.77731 11.8548C1.37424 12.2629 0.714234 12.2629 0.306118 11.8548C-0.101968 11.4517 -0.101968 10.7917 0.306118 10.3836L6.20637 4.47333C6.40289 4.27681 6.66483 4.16602 6.94207 4.16602C7.21932 4.16602 7.48126 4.27681 7.67777 4.46832L13.5881 10.3886C13.7896 10.5901 13.8904 10.857 13.8904 11.1243C13.8904 11.3912 13.7896 11.6584 13.5881 11.86C13.18 12.268 12.525 12.268 12.1169 11.86L6.94214 6.68Z" fill="#F8F8F8" />
+			</g>
+			<defs>
+				<clipPath id="clip0_71_2697">
+					<rect width="14" height="14" fill="white" transform="translate(0 14.4995) rotate(-90)" />
+				</clipPath>
+			</defs>
+		</svg>
+
+	</button>
 <?php endif ?>
 
 
